0fd086013a434e4936603ef6ece090d6d96e1496
Michael Haigh <mahaigh@gmail.com>
2020-02-21 17:08:23 -0500

    added url function

--- entrypoint/calm.py
+++ entrypoint/calm.py
@@ -10,10 +10,29 @@ import getpass
 from base64 import b64encode
 from requests.auth import HTTPBasicAuth
 
+sys.path.append(os.path.join(os.getcwd(), "nutest_gcp.egg"))
+
+from framework.lib.nulog import INFO, ERROR
 from helpers.rest import (RequestParameters, PostRequestParameters,
      RequestResponse, RESTClient, PostRESTClient)
 
+
+def create_v3_url(ip, endpoint):
+
+  return f"https://{ip}:9440/api/nutanix/v3/{endpoint}"
+
 def main():
+
+  # Get and log the config from the Env variable
+  config = json.loads(os.environ["CUSTOM_SCRIPT_CONFIG"])
+  INFO(config)
+
+  # Get PC info from the config dict
+  pc_info = config.get("my_pc")
+  pc_external_ip = pc_info.get("ips")[0][0]
+  pc_internal_ip = pc_info.get("ips")[0][1]
+  pc_password = pc_info.get("prism_password")
+
 
   parameters = RequestParameters(
         uri="url",
@@ -21,7 +40,7 @@ def main():
         password="password"
   )
 
-  print("Success!")
+  print(create_v3_url(pc_external_ip, "projects"))
 
 if __name__ == '__main__':
   main()
