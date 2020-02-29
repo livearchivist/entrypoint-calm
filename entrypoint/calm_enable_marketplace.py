9f0e8c306c71bbc31bbe7742b5731712f6474bd5
Michael Haigh <mahaigh@gmail.com>
2020-02-29 16:45:32 -0500

    added marketplace enable

---
+++ entrypoint/calm_enable_marketplace.py
@@ -0,0 +1,56 @@
+"""
+calm_enable_marketplace.py: automation to enable the
+Nutanix Marketplace on NX-on-GCP / Test Drive.
+
+Author: michael@nutanix.com
+Date:   2020-02-29
+"""
+
+import sys
+import os
+import json
+
+sys.path.append(os.path.join(os.getcwd(), "nutest_gcp.egg"))
+
+from framework.lib.nulog import INFO, ERROR
+from helpers.rest import RequestResponse
+from helpers.calm import create_via_v3_post
+
+
+def main():
+
+  # Get and log the config from the Env variable
+  config = json.loads(os.environ["CUSTOM_SCRIPT_CONFIG"])
+  INFO(config)
+
+  # Get PC info from the config dict
+  pc_info = config.get("my_pc")
+  pc_external_ip = pc_info.get("ips")[0][0]
+  pc_internal_ip = pc_info.get("ips")[0][1]
+  pc_password = pc_info.get("prism_password")
+
+  try:
+
+    # Create the API call body
+    body = {
+      "create_via_v3_post": True
+    }
+
+    # Make API call to enable marketplace
+    resp = create_via_v3_post(pc_external_ip,
+                              "marketplace_items/config",
+                              pc_password, body)
+
+    # Log appropriately based on response
+    if (resp.code == 200 or resp.code == 202):
+      INFO("Marketplace enabled successfully.")
+    else:
+      raise Exception(f"Marketplace enable failed with:\n" +
+                      f"Error Code: {resp.code}\n" +
+                      f"Error Message: {resp.message}")
+
+  except Exception as ex:
+    INFO(ex)
+
+if __name__ == '__main__':
+  main()
