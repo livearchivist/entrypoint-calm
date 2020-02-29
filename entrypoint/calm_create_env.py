c0a7b6edf4f01a8ed2bb72fbfde1bb2399c931b7
Michael Haigh <mahaigh@gmail.com>
2020-02-28 19:42:07 -0500

    Fixed bugs with env create

--- entrypoint/calm_create_env.py
+++ entrypoint/calm_create_env.py
@@ -15,7 +15,8 @@ sys.path.append(os.path.join(os.getcwd(), "nutest_gcp.egg"))
 
 from framework.lib.nulog import INFO, ERROR
 from helpers.calm import (file_to_dict, uuid_via_v3_post,
-                          body_via_v3_get, body_via_v3_post)
+                          body_via_v3_get, body_via_v3_post,
+                          create_via_v3_post)
 
 
 def main():
@@ -62,11 +63,11 @@ def main():
     INFO(f"image_uuid: {image_uuid}")
         
     # Generate UUIDs for new components
-    env_name = uuid.uuid4()
-    env_uuid = uuid.uuid4()
-    substrate_uuid = uuid.uuid4()
-    key_uuid = uuid.uuid4()
-    pass_uuid = uuid.uuid4()
+    env_name = str(uuid.uuid4())
+    env_uuid = str(uuid.uuid4())
+    substrate_uuid = str(uuid.uuid4())
+    key_uuid = str(uuid.uuid4())
+    pass_uuid = str(uuid.uuid4())
 
     # Sub in our UUIDs and names:
     # env
@@ -86,7 +87,7 @@ def main():
     # subnet
     env_spec["spec"]["resources"]["substrate_definition_list"][0]\
             ["create_spec"]["resources"]["nic_list"][0]\
-            ["uuid"] = subnet_uuid
+            ["subnet_reference"]["uuid"] = subnet_uuid
 
     # image
     env_spec["spec"]["resources"]["substrate_definition_list"][0]\
@@ -107,16 +108,17 @@ def main():
             ["uuid"] = pass_uuid
 
     # key credential
-    env_spec["spec"]["resources"]["credential_definition_list"][0]\
+    env_spec["spec"]["resources"]["credential_definition_list"][1]\
             ["name"] = key_spec["name"]
-    env_spec["spec"]["resources"]["credential_definition_list"][0]\
+    env_spec["spec"]["resources"]["credential_definition_list"][1]\
             ["username"] = key_spec["username"]
-    env_spec["spec"]["resources"]["credential_definition_list"][0]\
+    env_spec["spec"]["resources"]["credential_definition_list"][1]\
             ["secret"]["value"] = key_spec["secret"]
-    env_spec["spec"]["resources"]["credential_definition_list"][0]\
+    env_spec["spec"]["resources"]["credential_definition_list"][1]\
             ["uuid"] = key_uuid
 
     # Make the API call to create the environment
+    INFO(f"env_spec: {env_spec}")
     resp = create_via_v3_post(pc_external_ip, "environments",
                               pc_password, env_spec)
 
