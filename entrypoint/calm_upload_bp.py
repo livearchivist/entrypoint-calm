670684d72272352799637172024498b2c1ec4e61
Michael Haigh <mahaigh@gmail.com>
2020-03-06 15:24:26 -0500

    added bp upload functionality

---
+++ entrypoint/calm_upload_bp.py
@@ -0,0 +1,85 @@
+"""
+calm_upload_bp.py: automation to upload
+blueprints on NX-on-GCP / Test Drive.
+
+Author: michael@nutanix.com
+Date:   2020-03-05
+"""
+
+import sys
+import os
+import json
+
+sys.path.append(os.path.join(os.getcwd(), "nutest_gcp.egg"))
+
+from framework.lib.nulog import INFO, ERROR
+from helpers.rest import RequestResponse
+from helpers.calm import (file_to_dict, uuid_via_v3_post,
+                          upload_bp_via_v3_post,
+                          get_subnet_info)
+
+
+def main(blueprint_name):
+
+  # Get and log the config from the Env variable
+  config = json.loads(os.environ["CUSTOM_SCRIPT_CONFIG"])
+  INFO(config)
+
+  # Get PC info from the config dict
+  pc_info = config.get("tdaas_pc")
+  pc_external_ip = pc_info.get("ips")[0][0]
+  pc_internal_ip = pc_info.get("ips")[0][1]
+  pc_password = pc_info.get("prism_password")
+
+  try:
+
+    # Read in the spec files and conver to dicts
+    subnet_spec = file_to_dict("calm_subnet.spec")
+    INFO(f"subnet_spec: {subnet_spec}")
+    bp_spec = file_to_dict("calm_mp_apps.spec")
+    INFO(f"bp_spec: {bp_spec}")
+
+    # Get our subnet info from the infra
+    subnet_info = get_subnet_info(pc_external_ip, pc_password,
+                                  subnet_spec["vlan"])
+
+    # Loop through the blueprints to upload
+    for bp in bp_spec["entities"]:
+
+      # Get our project uuid and create our payload
+      project_uuid = uuid_via_v3_post(pc_external_ip, "projects",
+                                      pc_password, bp["bp_project"])
+      payload = {
+        "name": bp["bp_name"],
+        "project_uuid": project_uuid
+      }
+
+      # Upload our blueprint
+      resp = upload_bp_via_v3_post(pc_external_ip, pc_password,
+                                   payload, bp["bp_file"])
+
+      # Need to re-upload to make ACTIVE
+      #project_spec["spec"]["name"] = project_name
+      #project_spec["spec"]["resources"]["subnet_reference_list"][0]\
+      #            ["name"] = subnet_info["name"]
+      #project_spec["spec"]["resources"]["subnet_reference_list"][0]\
+      #            ["uuid"] = subnet_info["uuid"]
+      #INFO(f"project_spec post-update: {project_spec}")
+
+      # Log appropriately based on response
+      if (resp.code == 200 or resp.code == 202):
+        INFO(f"{bp['bp_name']} blueprint created successfully.")
+      else:
+        raise Exception(f"{bp['bp_name']} blueprint create" +
+                        f" failed with:\n" +
+                        f"Error Code: {resp.code}\n" +
+                        f"Error Message: {resp.message}")
+
+  except Exception as ex:
+    INFO(ex)
+
+if __name__ == '__main__':
+  for project in sys.argv:
+    if not project.endswith("py"):
+      main(project)
+
