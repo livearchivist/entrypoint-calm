2161b5a23d0ec12531bf9be6dd3c1bdfcb758c66
Michael Haigh <mahaigh@gmail.com>
2020-03-17 21:03:01 -0400

    Increased sleep timer for windows image to be created

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -15,9 +15,9 @@ pip3 install -r requirements.txt
 execute_command "export NUTEST_PATH=/home"
 python3 calm_create_image.py
 #python3 calm_enable_marketplace.py
-python3 calm_create_project.py NY_Office LA_Office
 python3 calm_upload_icons.py
-sleep 600 # sleep so the image can be created
+sleep 1500 # sleep so the image can be created
+python3 calm_create_project.py NY_Office LA_Office
 python3 calm_create_env.py
 python3 calm_configure_project.py
 python3 calm_upload_bp.py
