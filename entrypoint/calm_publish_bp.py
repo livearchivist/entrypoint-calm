a5e4649ff044f93c855896bb723a8985bda9d8fc
Michael Haigh <mahaigh@gmail.com>
2020-04-01 12:09:57 -0400

    Moved BP description around to fix app description issue

--- entrypoint/calm_publish_bp.py
+++ entrypoint/calm_publish_bp.py
@@ -61,14 +61,13 @@ def main():
 
       # Modify our body
       bp["spec"]["name"] = publish["mp_name"]
-      bp["spec"]["description"] = publish["mp_description"]
       bp["status"]["name"] = publish["mp_name"]
-      #bp["status"]["description"] = publish["mp_description"]
       spec = copy.deepcopy(bp["spec"])
       status = copy.deepcopy(bp["status"])
       del bp["spec"]["resources"]
       del bp["status"]
       bp["metadata"]["kind"] = "marketplace_item"
+      bp["spec"]["description"] = publish["mp_description"]
       bp["spec"]["resources"] = {
         "app_attribute_list": ["FEATURED"]
       }
