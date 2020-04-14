8daa5971190644a8f60d1d459afe2edc63411705
Michael Haigh <mahaigh@gmail.com>
2020-04-14 10:27:22 -0400

    Initial Core AOS commit

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -14,17 +14,10 @@ yum -y install $(cat yum_pkgs.txt)
 pip3 install -r requirements.txt
 execute_command "export NUTEST_PATH=/home"
 python3 pc_create_image.py
-python3 calm_upload_icons.py
 sleep 1200 # sleep so the image can be created
-python pc_deploy_autodc.py
-python3 calm_create_project.py NY_Office LA_Office
 python3 calm_create_env.py
 python3 calm_configure_project.py
 python3 calm_upload_bp.py
 python3 calm_configure_bp.py
 python3 calm_launch_bp.py
-python3 calm_publish_bp.py
-python3 calm_approve_bp.py
-python3 calm_publish_apps.py
-python3 pc_authconfig.py
-sleep 1000
+sleep 600
