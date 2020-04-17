356630ba21d4f56f788335bee707ca7463351dc7
Michael Haigh <mahaigh@gmail.com>
2020-04-17 16:11:48 -0400

    Testing Karbon 2.0 container seeding

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -16,21 +16,13 @@ execute_command "export NUTEST_PATH=/home"
 python3 pc_create_image.py
 python3 pc_ntp.py
 python3 pe_ntp.py
-python3 calm_upload_icons.py
-python3 calm_enable_marketplace.py
-python3 calm_enable_showback.py
 sleep 1200 # sleep so the image can be created
 sh create_ssh_keys.sh
 python3 pc_ssh_keys.py
-python pc_deploy_autodc.py
-python3 calm_create_project.py NY_Office LA_Office
-python3 calm_create_env.py
-python3 calm_configure_project.py
-python3 calm_upload_bp.py
-python3 calm_configure_bp.py
-python3 calm_launch_bp.py
-python3 calm_publish_bp.py
-python3 calm_approve_bp.py
-python3 calm_publish_apps.py
-python3 pc_authconfig.py
-sleep 1000
+sh copy_karbon_bits.sh
+#python3 calm_create_env.py
+#python3 calm_configure_project.py
+#python3 calm_upload_bp.py
+#python3 calm_configure_bp.py
+#python3 calm_launch_bp.py
+#sleep 1000
