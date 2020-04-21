f86612ae89e1c5735ee9399ed3c97330cebc92e9
Michael Haigh <mahaigh@gmail.com>
2020-04-21 12:58:43 -0400

    Fixing upload json bug

--- entrypoint/helpers/calm.py
+++ entrypoint/helpers/calm.py
@@ -41,6 +41,16 @@ def create_v1_url(ip, endpoint):
     return f"https://{ip}:9440/PrismGateway/services/rest/v1/{endpoint}"
 
 
+def recursive_dict_lookup(dictionary, array):
+    """Given a dictionary, and an array of dictionary keys, return the
+       corresponding value of the dictionary"""
+    key = array.pop(0)
+    if len(array) > 0:
+        return recursive_dict_lookup(dictionary[key], array)
+    else:
+        return dictionary[key]
+
+
 def create_proxy_array(public_uvms):
     """Given a Proxy VM dictionary, return an array of tuples"""
 
