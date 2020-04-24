a67570a2077a4476d88c6141bf0eacac91b61d12
Michael Haigh <mahaigh@gmail.com>
2020-04-24 18:59:10 -0400

    Updated from master

--- manage-nx-on-gcp.py
+++ manage-nx-on-gcp.py
@@ -28,9 +28,9 @@ from datetime import datetime
 
 # ================== MODIFY FOR YOUR ENVIRONMENT ==================
 metadata = {
-    "projects": ["nutanix-expo"],  # replace with your project name
+    "projects": ["nutanix-expo"],  # replace with your project ID
     "duration": 24,  # replace with your desired number of hours
-    "name": time.strftime("%Y-%m-%d %H:%M:%S")  # optionally change
+    "name": time.strftime("%Y-%m-%d %H:%M:%S"),  # optionally change
 }
 
 service_name = "$MODERN-DC-TESTDRIVE-DEV"
@@ -41,10 +41,7 @@ service_name = "$MODERN-DC-TESTDRIVE-DEV"
 
 
 # The headers should not need to be modified
-headers = {
-    "request-type": "SERVICE",
-    "request-service-name": service_name
-}
+headers = {"request-type": "SERVICE", "request-service-name": service_name}
 
 
 # Given a filename, return a dict of the file's contents
@@ -57,25 +54,36 @@ def file_to_dict(filename):
 def print_help():
     print("To deploy a cluster, specify a json template file:")
     print("  python3 manage-nx-on-gcp.py path/to/cluster-spec.json\n")
-    print("To get all cluster deployment info, specify the request_id " +
-          "UUID and append .info:")
+    print(
+        "To get all cluster deployment info, specify the request_id "
+        + "UUID and append .info:"
+    )
     print("  python3 manage-nx-on-gcp.py <request_id-uuid>.info\n")
-    print("To get only cluster IP/password info, specify the request_id" +
-          "UUID and append .pass:")
+    print(
+        "To get only cluster IP/password info, specify the request_id"
+        + "UUID and append .pass:"
+    )
     print("  python3 manage-nx-on-gcp.py <request_id-uuid>.pass\n")
-    print("To modify a cluster's duration, specify the request_id " +
-          "UUID and append .mod:")
+    print(
+        "To modify a cluster's duration, specify the request_id "
+        + "UUID and append .mod:"
+    )
     print("  python3 manage-nx-on-gcp.py <request_id-uuid>.mod\n")
-    print("To get cluster deployment logs, specify the request_id " +
-          "UUID and append .logs:")
+    print(
+        "To get cluster deployment logs, specify the request_id "
+        + "UUID and append .logs:"
+    )
     print("  python3 manage-nx-on-gcp.py <request_id-uuid>.logs\n")
-    print("To delete a cluster, specify the request_id " +
-          "UUID and append .del:")
+    print("To delete a cluster, specify the request_id " + "UUID and append .del:")
     print("  python3 manage-nx-on-gcp.py <request_id-uuid>.del\n")
-    print("To use your most recently created cluster, use the following" +
-          " tail+awk command:")
-    print("  python3 manage-nx-on-gcp.py $(tail -1 requests.ids | awk " +
-          "{'print $NF'}).info\n")
+    print(
+        "To use your most recently created cluster, use the following"
+        + " tail+awk command:"
+    )
+    print(
+        "  python3 manage-nx-on-gcp.py $(tail -1 requests.ids | awk "
+        + "{'print $NF'}).info\n"
+    )
 
 
 # Output the cluster info
@@ -91,23 +99,41 @@ def info(cluster, detail):
 
         # Make sure it wasn't a non-VPN call
         if str(resp.content).startswith("b'<!DOCTYPE html>"):
-            sys.exit("Error: You must be on full-tunnel VPN to use this " +
-                     "script.")
+            sys.exit("Error: You must be on full-tunnel VPN to use this " + "script.")
 
         # Print the info
         cluster_info = json.loads(resp.content.decode("utf-8"))
         if detail == "short":
-            print(json.dumps(cluster_info["data"]["metadata"],
-                             indent=4, sort_keys=True))
+            print(
+                json.dumps(cluster_info["data"]["metadata"], indent=4, sort_keys=True)
+            )
         elif detail == "pass":
-            pc_info = cluster_info["data"]["data"]["allocated_resources"]\
-                                  ["tdaas_pc"]
-            pe_info = cluster_info["data"]["data"]["allocated_resources"]\
-                                  ["tdaas_cluster"]
-            print(f'PC Info:\thttps://{pc_info["external_ip"]}:9440\t\t' +
-                  f'{pc_info["prism_password"]}\n' +
-                  f'PE Info:\thttps://{pe_info["external_ip"]}:9440\t\t' +
-                  f'{pe_info["prism_password"]}')
+            if "tdaas_pc" in cluster_info["data"]["data"]["allocated_resources"]:
+                pc_info = cluster_info["data"]["data"]["allocated_resources"][
+                    "tdaas_pc"
+                ]
+                print(
+                    'PC Info:\thttps://{pc_info["external_ip"]}:9440\t\t'
+                    + f'{pc_info["prism_password"]}'
+                )
+            pe_info = cluster_info["data"]["data"]["allocated_resources"][
+                "tdaas_cluster"
+            ]
+            print(
+                f'PE Info:\thttps://{pe_info["external_ip"]}:9440\t\t'
+                + f'{pe_info["prism_password"]}'
+            )
+            if "proxy_vm" in cluster_info["data"]["data"]["allocated_resources"]:
+                for uvm in cluster_info["data"]["data"]["allocated_resources"][
+                    "proxy_vm"
+                ]["target"]:
+                    uvm_dict = cluster_info["data"]["data"]["allocated_resources"][
+                        "proxy_vm"
+                    ]["target"][uvm]
+                    print(
+                        f'{uvm}:\t{uvm_dict["external_ip"]}\t\t'
+                        + f'{uvm_dict["internal_ip"]}'
+                    )
         else:
             print(json.dumps(cluster_info, indent=4, sort_keys=True))
 
@@ -124,10 +150,10 @@ def create(cluster):
     cluster_spec = file_to_dict(cluster)
 
     # Create the payload
-    payload = {
-        "resource_specs": cluster_spec,
-        "metadata": metadata
-    }
+    if "resource_specs" in cluster_spec:
+        payload = cluster_spec
+    else:
+        payload = {"resource_specs": cluster_spec, "metadata": metadata}
 
     # Create the URL and make the call
     url = "https://nx-gcp.nutanix.com/api/v1/deployments/requests"
@@ -138,8 +164,7 @@ def create(cluster):
 
         # Make sure it wasn't a non-VPN call
         if str(resp.content).startswith("b'<!DOCTYPE html>"):
-            sys.exit("Error: You must be on full-tunnel VPN to use this " +
-                     "script.")
+            sys.exit("Error: You must be on full-tunnel VPN to use this " + "script.")
 
         # Set the response as a dictionary (return is a byte)
         request_info = json.loads(resp.content.decode("utf-8"))
@@ -147,20 +172,22 @@ def create(cluster):
 
         # Add the request id to the file for usage later
         f = open("requests.ids", "a")
-        f.write(
-            f"{metadata['name']}:    {request_info['data']['request_id']}\n")
+        f.write(f"{metadata['name']}:    {request_info['data']['request_id']}\n")
         f.close()
         print("The request_id has been added to your 'requests.ids' file.\n")
 
         # Call the info function to get additional detail on the deployment
         time.sleep(5)
-        info(request_info['data']['request_id'], "short")
+        info(request_info["data"]["request_id"], "short")
 
     # Handle failure
     else:
         print("Request failed with the following detail:")
-        print(json.dumps(json.loads(resp.content.decode("utf-8")),
-                         indent=4, sort_keys=True))
+        print(
+            json.dumps(
+                json.loads(resp.content.decode("utf-8")), indent=4, sort_keys=True
+            )
+        )
 
 
 # Modify a cluster's duration time
@@ -175,8 +202,7 @@ def modify(cluster):
     # Get users input for how long to extend the cluster
     while True:
         try:
-            hours = int(
-                input(f"Enter the number of hours to extend request: "))
+            hours = int(input(f"Enter the number of hours to extend request: "))
             if hours > 720 or hours < 1:
                 print("Please enter in a number between 1 and 720, inclusive.")
                 continue
@@ -186,13 +212,13 @@ def modify(cluster):
             continue
 
     # Create the payload
-    payload = {
-        "duration": hours
-    }
+    payload = {"duration": hours}
 
     # Create the URL and make the call
-    url = f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}" +\
-        "/modify_duration"
+    url = (
+        f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}"
+        + "/modify_duration"
+    )
     resp = requests.post(url, json=payload, headers=headers)
 
     # Handle success or failure
@@ -211,8 +237,10 @@ def logs(cluster):
 
     # Create the URL and make the call
     req_id = cluster.split(".")[0]
-    url = f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}" +\
-          f"/plugins/logs"
+    url = (
+        f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}"
+        + f"/plugins/logs"
+    )
     resp = requests.get(url, allow_redirects=True, headers=headers)
 
     # Download the file if successful
@@ -236,8 +264,7 @@ def delete(cluster):
 
     # Make sure the user really wants to delete the cluster
     while True:
-        yesno = input("Are you sure you want to delete this cluster?" +
-                      " [y]es/[n]o: ")
+        yesno = input("Are you sure you want to delete this cluster?" + " [y]es/[n]o: ")
         if yesno.lower() == "n" or yesno.lower() == "no":
             print("Deletion cancelled. Exiting.")
             return
@@ -247,8 +274,9 @@ def delete(cluster):
         break
 
     # Create the URL and make the call
-    url = f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}" +\
-        "/release"
+    url = (
+        f"https://nx-gcp.nutanix.com/api/v1/deployments/requests/{req_id}" + "/release"
+    )
     resp = requests.post(url, headers=headers)
 
     # Handle success or failure
@@ -263,19 +291,21 @@ def delete(cluster):
 
 
 # Main function, handle inputs and call correct functions
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # Error out if no spec file was passed via CLI
     if len(sys.argv) == 1:
-        print("Error: please specify at least one argument when running " +
-              "this script.  Examples:\n")
+        print(
+            "Error: please specify at least one argument when running "
+            + "this script.  Examples:\n"
+        )
         print_help()
 
     # Print script info if "help" is passed
-    elif sys.argv[1].lower().endswith("help") or\
-            sys.argv[1].lower() == "-h":
-        print("Use this script to deploy, manage, and reclaim NX-on-GCP " +
-              "clusters.\n")
+    elif sys.argv[1].lower().endswith("help") or sys.argv[1].lower() == "-h":
+        print(
+            "Use this script to deploy, manage, and reclaim NX-on-GCP " + "clusters.\n"
+        )
         print_help()
 
     # Create an NX-on-GCP cluster for each cluster spec file
@@ -302,3 +332,4 @@ if __name__ == '__main__':
                 else:
                     print("Error: you did not specify a valid argument.\n")
                     print_help()
+
