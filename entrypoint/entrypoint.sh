d928cfc7afe5c485caab8ab3783b51321d017cd6
Michael Haigh <mahaigh@gmail.com>
2020-05-12 09:32:33 -0400

    Adding pe_user_add and exit code support

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -18,6 +18,7 @@ python3 pc_create_image.py
 python3 pc_ntp.py
 python3 pe_ntp.py
 sleep 1200 # sleep so the image can be created
+python3 pe_add_users.py
 sh create_ssh_keys.sh
 python3 pc_ssh_keys.py
 sh copy_karbon_bits.sh
@@ -33,4 +34,4 @@ python3 calm_launch_bp.py calm_bp_launch_depend.json
 python3 calm_delete_apps.py
 python3 calm_delete_bps.py
 sleep 1500
-#python3 calm_check_apps.py
+python3 calm_check_apps.py
