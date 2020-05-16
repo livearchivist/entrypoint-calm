88ad748c5d5af3f90e7c98becbcf3401e5d0c7f9
Michael Haigh <mahaigh@gmail.com>
2020-05-16 15:28:52 -0400

    Fixing container urls

--- entrypoint/copy_new_karbon_bits.sh
+++ entrypoint/copy_new_karbon_bits.sh
@@ -12,10 +12,10 @@ ssh nutanix@$PC_IP 'source /etc/profile; genesis stop karbon_core karbon_ui'
 sleep 30
 
 # Copy over the containers
-ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_core/karbon-core.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-core.tar.xz'
-ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_ui/karbon-ui.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-ui.tar.xz'
+ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_core/karbon-core.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-core.tar.xz.new'
+ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_ui/karbon-ui.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-ui.tar.xz.new'
 
-# Remove the old containers
+# Remove the old docker images
 ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-core | awk '{print \$3}')"
 ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-ui | awk '{print \$3}')"
 
