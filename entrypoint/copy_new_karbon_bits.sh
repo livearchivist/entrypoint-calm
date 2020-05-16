d89976e71cb286aa113c5a2c597ea0f9e43e27fb
Michael Haigh <mahaigh@gmail.com>
2020-05-16 13:52:55 -0400

    Adding sleep and re-arranging commands

--- entrypoint/copy_new_karbon_bits.sh
+++ entrypoint/copy_new_karbon_bits.sh
@@ -9,14 +9,15 @@ echo $PC_IP
 
 # Stop karbon_core and karbon_ui services
 ssh nutanix@$PC_IP 'source /etc/profile; genesis stop karbon_core karbon_ui'
-
-# Remove the old containers
-ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-core | awk '{print \$3}')"
-ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-ui | awk '{print \$3}')"
+sleep 30
 
 # Copy over the containers
 ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_core/karbon-core.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-core.tar.xz'
 ssh nutanix@$PC_IP 'sudo wget -O /home/docker/karbon_ui/karbon-ui.tar.xz https://storage.googleapis.com/testdrive-templates/library/release/karbon/karbon-ui.tar.xz'
+
+# Remove the old containers
+ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-core | awk '{print \$3}')"
+ssh nutanix@$PC_IP "docker image rm \$(docker image ls | grep karbon-ui | awk '{print \$3}')"
 
 # Start the services back up
 ssh nutanix@$PC_IP 'source /etc/profile; cluster start'
