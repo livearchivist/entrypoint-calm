5b861f71a147699310f317c7f102800079c8ffe5
Michael Haigh <mahaigh@gmail.com>
2020-05-19 09:55:17 -0400

    Pulling from flow branch

--- entrypoint/nuclei_enable_flow.sh
+++ entrypoint/nuclei_enable_flow.sh
@@ -7,5 +7,14 @@ PC_IP="${PC_IP%\"}"
 PC_IP="${PC_IP#\"}"
 echo $PC_IP
 
+# Do the same for the PC Password
+PC_PASS=$(jq '.tdaas_pc.prism_password' /home/config.json)
+PC_PASS="${PC_PASS%\"}"
+PC_PASS="${PC_PASS#\"}"
+echo $PC_PASS
+
+# Add PC Key to known_hosts
+ssh-keyscan $PC_IP | grep nistp521 > /root/.ssh/known_hosts
+
 # Enable Flow
-ssh nutanix@$PC_IP 'source /etc/profile; nuclei microseg.enable'
+<&- ssh nutanix@$PC_IP "bash -lc 'nuclei -server localhost -username admin -password $PC_PASS microseg.enable'"
