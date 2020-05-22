b8a04ebdcddad4add4d80daf14420e6badb60dab
Michael Haigh <mahaigh@gmail.com>
2020-05-22 14:14:49 -0400

    Creating Branch to fix Calm 2.9.1 env changes

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -14,25 +14,7 @@ echo ${CUSTOM_SCRIPT_CONFIG}
 yum -y install epel-release
 yum -y install $(cat yum_pkgs.txt)
 pip3 install -r requirements.txt
-execute_command "export NUTEST_PATH=/home"
 python3 pc_create_image.py
-python3 pc_ntp.py
-python3 pe_ntp.py
-sleep 1200 # sleep so the image can be created
-sh create_ssh_keys.sh
-python3 pc_ssh_keys.py
-python pc_deploy_autodc.py
-python3 calm_create_project.py NY_Office LA_Office
+sleep 300
 python3 calm_create_env.py
 python3 calm_configure_project.py
-python3 calm_upload_bp.py
-python3 calm_configure_bp.py
-python3 calm_launch_bp.py calm_bp_launch.json
-python3 calm_publish_bp.py
-python3 calm_approve_bp.py
-python3 calm_publish_apps.py
-python3 pc_authconfig.py
-sleep 1200
-python3 calm_launch_bp.py calm_bp_launch_depend.json
-sleep 1200
-python3 calm_check_apps.py
