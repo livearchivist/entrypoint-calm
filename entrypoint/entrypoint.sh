17d5c02d5dc1b857bac028e1f0ad79a9707730cc
Michael Haigh <mahaigh@gmail.com>
2020-05-22 10:27:10 -0400

    Implemented check_apps functionality

--- entrypoint/entrypoint.sh
+++ entrypoint/entrypoint.sh
@@ -17,10 +17,11 @@ execute_command "export NUTEST_PATH=/home"
 python3 pc_create_image.py
 python3 pc_ntp.py
 python3 pe_ntp.py
-sleep 1560 # sleep so the image can be created
+sleep 1200 # sleep so the image can be created
 python3 calm_create_env.py
 python3 calm_configure_project.py
 python3 calm_upload_bp.py
 python3 calm_configure_bp.py
 python3 calm_launch_bp.py calm_bp_launch.json
-sleep 300
+sleep 4800
+python3 calm_check_apps.py
