25d54bdd0955ea3d3bcfe5447647807cb0607422
Michael Haigh <mahaigh@gmail.com>
2020-06-09 14:47:54 -0400

    Pulled in new helper functions

--- entrypoint/helpers/calm.py
+++ entrypoint/helpers/calm.py
@@ -6,35 +6,64 @@ Author: michael@nutanix.com
 Date:   2020-02-24
 """
 
+from helpers.rest import RequestParameters, RequestResponse, RESTClient
 import sys
 import os
 import requests
 import urllib3
 import json
 
-from helpers.rest import RequestParameters, RequestResponse, RESTClient
-
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 
-# Given a filename, return a dict of the file's contents
+def file_to_string(filename):
+    """Given a filename, return a str of the file's contents"""
+    with open(os.path.join(os.getcwd(), filename)) as str_file:
+        return str_file.read().rstrip("\n")
+
+
 def file_to_dict(filename):
+    """Given a filename, return a dict of the file's contents"""
     with open(os.path.join(os.getcwd(), filename)) as json_file:
         return json.load(json_file)
 
 
-# Given an IP and Endpoint, return Nutanix v3 API URL
 def create_v3_url(ip, endpoint):
+    """Given an IP and Endpoint, return Nutanix v3 API URL"""
     return f"https://{ip}:9440/api/nutanix/v3/{endpoint}"
 
 
-# Given an IP and  Endpoint, return Nutanix v1 API URL
 def create_v1_url(ip, endpoint):
+    """Given an IP and  Endpoint, return Nutanix v1 API URL"""
     return f"https://{ip}:9440/PrismGateway/services/rest/v1/{endpoint}"
 
 
-# Create a new entity via a v1 post call, return the response
+def recursive_dict_lookup(dictionary, array):
+    """Given a dictionary, and an array of dictionary keys, return the
+       corresponding value of the dictionary"""
+    key = array.pop(0)
+    if len(array) > 0:
+        return recursive_dict_lookup(dictionary[key], array)
+    else:
+        return dictionary[key]
+
+
+def create_proxy_array(public_uvms):
+    """Given a Proxy VM dictionary, return an array of tuples"""
+
+    uvm_array = []
+    for uvm in public_uvms["public_uvms"]:
+        uvm_array.append(
+            (
+                public_uvms["public_uvms"][uvm]["external_ip"],
+                public_uvms["public_uvms"][uvm]["internal_ip"],
+            )
+        )
+    return uvm_array
+
+
 def create_via_v1_post(ip, endpoint, password, body):
+    """Create a new entity via a v1 post call, return the response"""
 
     # Make the API call
     parameters = RequestParameters(
@@ -52,8 +81,8 @@ def create_via_v1_post(ip, endpoint, password, body):
     return resp
 
 
-# Create a new entity via a v3 post call, return the response
 def create_via_v3_post(ip, endpoint, password, body):
+    """Create a new entity via a v3 post call, return the response"""
 
     # Make the API call
     parameters = RequestParameters(
@@ -71,8 +100,8 @@ def create_via_v3_post(ip, endpoint, password, body):
     return resp
 
 
-# Uploads a named blueprint to a particular project
 def upload_bp_via_v3_post(ip, password, body, filename):
+    """Uploads a named blueprint to a particular project"""
 
     # Create the file dictionary
     files = {"file": open(f"blueprints/{filename}", "rb")}
@@ -93,8 +122,8 @@ def upload_bp_via_v3_post(ip, password, body, filename):
     return resp
 
 
-# Uploads an app_icon
 def upload_icon_via_v3_post(ip, password, body, icon):
+    """Uploads an app_icon"""
 
     # Create the file dictionary
     files = {"image": (icon["name"], open(f"images/{icon['file']}", "rb"), "image/png")}
@@ -115,9 +144,9 @@ def upload_icon_via_v3_post(ip, password, body, icon):
     return resp
 
 
-# Return the UUID of a desired entity.  If entity_name is empty
-# assume a single entity in response and send first UUID
 def uuid_via_v3_post(ip, endpoint, password, entity_name):
+    """Return the UUID of a desired entity.  If entity_name is empty
+     assume a single entity in response and send first UUID"""
 
     # Make the API call
     parameters = RequestParameters(
@@ -140,9 +169,9 @@ def uuid_via_v3_post(ip, endpoint, password, entity_name):
             return entity["metadata"]["uuid"]
 
 
-# Return the body of a group of entities
-# If payload is None, assume length=100
 def body_via_v3_post(ip, endpoint, password, payload):
+    """Return the body of a group of entities
+     If payload is None, assume length=100"""
 
     # Determine payload
     if payload is None:
@@ -165,8 +194,8 @@ def body_via_v3_post(ip, endpoint, password, payload):
     return resp
 
 
-# Return the body of a desired entity
 def body_via_v3_get(ip, endpoint, password, entity_uuid):
+    """Return the body of a desired entity"""
 
     # Make the API call
     parameters = RequestParameters(
@@ -185,8 +214,8 @@ def body_via_v3_get(ip, endpoint, password, entity_uuid):
     return resp
 
 
-# Update a given entity with a PUT
 def update_via_v3_put(ip, endpoint, password, entity_uuid, body):
+    """Update a given entity with a PUT"""
 
     # Make the API call
     parameters = RequestParameters(
@@ -205,9 +234,48 @@ def update_via_v3_put(ip, endpoint, password, entity_uuid, body):
     return resp
 
 
-# Given a VLAN ID, return a dict of the matching
-# Subnet UUID and name
+def update_via_v1_put(ip, endpoint, password, entity, body):
+    """Update a given entity with a PUT"""
+
+    # Make the API call
+    parameters = RequestParameters(
+        uri=create_v1_url(ip, f"{endpoint}/{entity}"),
+        username="admin",
+        password=password,
+        method="put",
+        payload=json.dumps(body),
+        files=None,
+    )
+    rest_client = RESTClient(parameters)
+    resp = rest_client.request()
+    print(f"update_via_v1_put: {ip}, {endpoint}, {entity}")
+
+    # Return the response
+    return resp
+
+
+def del_via_v3_delete(ip, endpoint, password, entity_uuid):
+    """Delete a given entity with a DELETE"""
+
+    # Make the API call
+    parameters = RequestParameters(
+        uri=create_v3_url(ip, f"{endpoint}/{entity_uuid}"),
+        username="admin",
+        password=password,
+        method="delete",
+        payload=None,
+        files=None,
+    )
+    rest_client = RESTClient(parameters)
+    resp = rest_client.request()
+    print(f"del_via_v3_delete: {ip}, {endpoint}, {entity_uuid}")
+
+    # Return the response
+    return resp
+
+
 def get_subnet_info(ip, password, vlan_id):
+    """Given a VLAN ID, return a dict of the matching Subnet UUID and name"""
 
     subnet_info = {}
 
@@ -219,4 +287,3 @@ def get_subnet_info(ip, password, vlan_id):
             subnet_info["uuid"] = subnet["metadata"]["uuid"]
 
     return subnet_info
-
